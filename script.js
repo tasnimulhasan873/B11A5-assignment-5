8d89497c310ac3a4cf32f64d3ff1f0c619cfa2f2
tasnimulhasan873 <tasnimul873@gmail.com>
2025-03-04 12:47:14 +0600

    color changes added

--- script.js
+++ script.js
@@ -7,3 +7,9 @@ function updateDateTime() {
 }
 setInterval(updateDateTime, 1000); 
 window.onload = updateDateTime;
+
+document.getElementById("colorChanger").addEventListener("click", function () {
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+  });
+
