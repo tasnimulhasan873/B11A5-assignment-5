c701779901652a49b3308df79dd1a8dcd452697e
tasnimulhasan873 <tasnimul873@gmail.com>
2025-03-04 15:07:40 +0600

    QA  part added

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function updateDateTime() {
-    const now = new Date();
-    const weekday = now.toLocaleString('en-US', { weekday: 'long' }) + ',';
-    const date = now.toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
-    document.getElementById('weekday').innerText = weekday;
-    document.getElementById('dateTime').innerText = date;
+  const now = new Date();
+  const weekday = now.toLocaleString("en-US", { weekday: "long" }) + ",";
+  const date = now.toLocaleString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  document.getElementById("weekday").innerText = weekday;
+  document.getElementById("dateTime").innerText = date;
 }
-setInterval(updateDateTime, 1000); 
+setInterval(updateDateTime, 1000);
 window.onload = updateDateTime;
 
 document.getElementById("colorChanger").addEventListener("click", function () {
-    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
-  });
-
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
+});
